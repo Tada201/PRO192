5f08cc5e4f8acbbb9d3a811208b265f145934375
ManhTung <90136516+Tada201@users.noreply.github.com>
2025-05-24 11:31:52 +0700

    minor changes to images media queries

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'Org_code/images/*',
-          dest: 'images',
-        },
-        {
-          src: 'Org_code/resource/*',
-          dest: 'resource',
-        },
-        {
-          src: 'Org_code/*.xlsx',
-          dest: '',
-        },
-      ],
-    }),
   ],
   optimizeDeps: {
     include: ['lucide-react'],
